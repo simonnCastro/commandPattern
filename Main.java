97ca0423a62977dc662710e9ef81693210fae44f
simonnCastro <61060425+simonnCastro@users.noreply.github.com>
2024-09-19 14:44:19 +0000

    modified and deleted files

--- Main.java
+++ Main.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class Main { 
     public static void main(String[] args) {
         Lights lights = new Lights();
+        MusicPlayer musicPlayer = new MusicPlayer();
+        Thermostat thermostat = new Thermostat();
         CenterHub rc = new CenterHub();
 
         ChangeBrightness changeBrightness= new ChangeBrightness(lights);
         System.out.println(changeBrightness.execute());
+
+        ChangePlaylist changePlaylist = new ChangePlaylist(musicPlayer);
+        System.out.println(changePlaylist.execute());
+        DecreaseVolume decreaseVolume = new DecreaseVolume(musicPlayer);
+        System.out.println(decreaseVolume.execute());
+
+        IncreaseTemperature increaseTemperature = new IncreaseTemperature(thermostat);
+        System.out.println(increaseTemperature.execute());
     }
 }
